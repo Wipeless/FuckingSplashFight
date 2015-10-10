5ee1b723aab514b7abaa3b7f6ef9de1e182452a7
Daniel Rohrlick <drohrlick@ucsd.edu>
2015-10-09 23:31:23 -0700

    Adding animations, user input
    Adding simple user input.
    Adding simple animations for scale.
    Added simple scene.

--- Assets/_Scripts/SceneManager.cs
+++ Assets/_Scripts/SceneManager.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class SceneManager : MonoBehaviour {
 
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
     public void OnClickMainMenu()
     {
         Application.LoadLevel(1);
