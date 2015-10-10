b6a2ca0186a7ce787170920e9c5987c36b747331
khelton <kevin.helton@hotmail.com>
2015-10-10 13:05:08 -0700

    material tile script

---
+++ Assets/_Materials/materialTile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class materialTile : MonoBehaviour {
+
+
+    public float scaleX = 1;
+    public float scaleY = 1;
+    int materialIndex = 0;
+    //public Vector2 uvAnimationRate = new Vector2(.05f, .025f);
+    string textureName = "_MainTex";
+
+    Vector2 uvOffset = Vector2.zero;
+    
+    // Use this for initialization
+    void Start () {
+        if (GetComponent<Renderer>().enabled)
+        {
+            GetComponent<Renderer>().materials[materialIndex].SetTextureScale(textureName, new Vector2(scaleX, scaleY));
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
