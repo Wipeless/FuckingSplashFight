1dbef3651c90b42080af7060bc5bca22926d7278
Daniel Rohrlick <drohrlick@ucsd.edu>
2015-10-11 02:26:17 -0700

    Initial Level Manager working
    Different levels loading.
    Need to test more triggers.
    populate environments

--- Assets/_Scripts/GameplayScripts/LevelManagerScript.cs
+++ Assets/_Scripts/GameplayScripts/LevelManagerScript.cs
@@ -25,17 +25,104 @@ public class LevelManagerScript : MonoBehaviour {
         LEVEL1,
         LEVEL2,
         LEVEL3,
+        FINAL,
     }
 
     public EnumLevels CurrentLevel = EnumLevels.LEVEL1;
 
+    public bool isRequestLevelChange = false;
+
 	// Use this for initialization
 	void Start () {
-	
+        LoadLevel1();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (TriggerBox_Level1_2.GetComponent<RoomTrigger>().CheckPointReached)
+        {
+            isRequestLevelChange = true;
+            CurrentLevel = EnumLevels.LEVEL2;
+        }
+        else if (TriggerBox_Level2_3.GetComponent<RoomTrigger>().CheckPointReached)
+        {
+            isRequestLevelChange = true;
+            CurrentLevel = EnumLevels.LEVEL3;
+        }
+        else if (TriggerBox_Level3_final.GetComponent<RoomTrigger>().CheckPointReached)
+        {
+            isRequestLevelChange = true;
+            CurrentLevel = EnumLevels.FINAL;
+        }
+
+        if (isRequestLevelChange)
+        {
+            isRequestLevelChange = false;
+
+            switch (CurrentLevel)
+            {
+                case EnumLevels.LEVEL1:
+                    LoadLevel1();
+                    break;
+                case EnumLevels.LEVEL2:
+                    LoadLevel2();
+                    break;
+                case EnumLevels.LEVEL3:
+                    LoadLevel3();
+                    break;
+                case EnumLevels.FINAL:
+                    //final level
+                    break;
+            }
+        }
 	}
+
+    private void LoadLevel1()
+    {
+        Doorway3.SetActive(true);
+        Room3.SetActive(true);
+        Door1_2_entry.SetActive(true);
+
+        Doorway1.SetActive(false);
+        Doorway2.SetActive(false);
+        Room1.SetActive(false);
+        Room2.SetActive(false);
+        Door1_2_exit.SetActive(false);
+        Door2_3_entry.SetActive(false);
+        Door2_3_exit.SetActive(false);
+        Door3_final.SetActive(false);
+    }
+
+    private void LoadLevel2()
+    {
+        Doorway2.SetActive(true);
+        Doorway3.SetActive(true);
+        Room2.SetActive(true);
+        Door1_2_exit.SetActive(true);
+        Door2_3_entry.SetActive(true);
+
+        Doorway1.SetActive(false);
+        Room1.SetActive(false);
+        Room3.SetActive(false);
+        Door1_2_entry.SetActive(false);
+        Door2_3_exit.SetActive(false);
+        Door3_final.SetActive(false);
+    }
+
+    private void LoadLevel3()
+    {
+        Doorway1.SetActive(true);
+        Doorway2.SetActive(true);
+        Room1.SetActive(true);
+        Door2_3_exit.SetActive(true);
+        Door3_final.SetActive(true);
+
+        Doorway3.SetActive(false);
+        Room2.SetActive(false);
+        Room3.SetActive(false);
+        Door1_2_entry.SetActive(false);
+        Door1_2_exit.SetActive(false);
+        Door2_3_entry.SetActive(false);
+    }
 }
