b0df9e7ea62cc8889b3a3a735347db0790a9c6c4
Daniel Rohrlick <drohrlick@ucsd.edu>
2015-10-11 01:27:14 -0700

    Populating Level Manager
    Setting up the rooms, doors, triggers, etc

--- Assets/_Scripts/GameplayScripts/LevelManagerScript.cs
+++ Assets/_Scripts/GameplayScripts/LevelManagerScript.cs
@@ -2,16 +2,23 @@
 
 public class LevelManagerScript : MonoBehaviour {
 
-    public GameObject Doorway1;
-    public GameObject Doorway2;
-    public GameObject Doorway3;
     public GameObject Room1;
     public GameObject Room2;
     public GameObject Room3;
+
+    public GameObject Doorway1;
+    public GameObject Doorway2;
+    public GameObject Doorway3;
+ 
     public GameObject Door1_2_entry;
     public GameObject Door1_2_exit;
     public GameObject Door2_3_entry;
     public GameObject Door2_3_exit;
+    public GameObject Door3_final;
+
+    public GameObject TriggerBox_Level1_2;
+    public GameObject TriggerBox_Level2_3;
+    public GameObject TriggerBox_Level3_final;
 
     public enum EnumLevels
     {
